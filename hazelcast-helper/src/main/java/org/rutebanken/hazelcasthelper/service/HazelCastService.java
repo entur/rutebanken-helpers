7fc803a86155d493cedab46b0b5f7cdd594d359d
Erlend Nossum <en@scienta.no>
2017-05-08 14:50:04 +0200

    NRP-1487 Moved comment to correct place

--- hazelcast-helper/src/main/java/org/rutebanken/hazelcasthelper/service/HazelCastService.java
+++ hazelcast-helper/src/main/java/org/rutebanken/hazelcasthelper/service/HazelCastService.java
@@ -134,11 +134,11 @@ public class HazelCastService {
                 .setJoin(joinCfg)
                 .setSSLConfig(new SSLConfig().setEnabled(false));
 
-        // http://docs.hazelcast.org/docs/3.7/manual/html-single/index.html#backing-up-maps
         MapConfig mapConfig = cfg.getMapConfig("default");
         updateDefaultMapConfig(mapConfig);
 
         log.info("Old config: b_count "+mapConfig.getBackupCount()+" async_b_count "+mapConfig.getAsyncBackupCount()+" read_backup_data "+mapConfig.isReadBackupData());
+        // http://docs.hazelcast.org/docs/3.7/manual/html-single/index.html#backing-up-maps
         mapConfig.setBackupCount( 2 )
                 .setAsyncBackupCount( 0 )
                 .setReadBackupData( true );
