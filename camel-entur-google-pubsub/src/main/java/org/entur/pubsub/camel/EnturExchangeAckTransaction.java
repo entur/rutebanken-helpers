55a734b81efbaf5ff3968222824fd5896a8f315e
vincent <vincent.paturet@entur.org>
2019-10-03 13:46:33 +0200

    ROR-647 Support for aggregated exchanges

--- camel-entur-google-pubsub/src/main/java/org/entur/pubsub/camel/EnturExchangeAckTransaction.java
+++ camel-entur-google-pubsub/src/main/java/org/entur/pubsub/camel/EnturExchangeAckTransaction.java
@@ -6,60 +6,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class EnturExchangeAckTransaction implements Synchronization {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-
+    /**
+     * Ack successful exchanges.
+     *
+     * @param exchange
+     */
     @Override
     public void onComplete(Exchange exchange) {
         logger.debug("Acknowledging message after successful processing for Exchange " + exchange.getExchangeId());
-        ack(getAckList(exchange));
+        getAck(exchange).ack();
     }
 
-
     /**
-     * Negatively ack failed messages.
+     * Negatively ack failed exchanges.
      *
      * @param exchange
      */
     @Override
     public void onFailure(Exchange exchange) {
         logger.debug("Acknowledging message after failed processing for Exchange " + exchange.getExchangeId());
-        nack(getAckList(exchange));
+        getAck(exchange).nack();
     }
 
-
-    private void ack(List<BasicAcknowledgeablePubsubMessage> ackList) {
-        for (BasicAcknowledgeablePubsubMessage ack : ackList) {
-            ack.ack();
-        }
-    }
-
-    private void nack(List<BasicAcknowledgeablePubsubMessage> ackList) {
-        for (BasicAcknowledgeablePubsubMessage ack : ackList) {
-            ack.nack();
-        }
-    }
-
-    private List<BasicAcknowledgeablePubsubMessage> getAckList(Exchange exchange) {
-        List<BasicAcknowledgeablePubsubMessage> ackList = new ArrayList<>();
-
-        if (null != exchange.getProperty(Exchange.GROUPED_EXCHANGE)) {
-            for (Exchange ex : (List<Exchange>) exchange.getProperty(Exchange.GROUPED_EXCHANGE, List.class)) {
-                BasicAcknowledgeablePubsubMessage ack = (BasicAcknowledgeablePubsubMessage) ex.getIn().getHeader(EnturGooglePubSubConstants.ACK_ID);
-                if (null != ack) {
-                    ackList.add(ack);
-                }
-            }
-        } else {
-            ackList.add((BasicAcknowledgeablePubsubMessage) exchange.getIn().getHeader(EnturGooglePubSubConstants.ACK_ID));
-        }
-
-        return ackList;
+    private BasicAcknowledgeablePubsubMessage getAck(Exchange exchange) {
+        return ((BasicAcknowledgeablePubsubMessage) exchange.getIn().getHeader(EnturGooglePubSubConstants.ACK_ID));
     }
 }
 
