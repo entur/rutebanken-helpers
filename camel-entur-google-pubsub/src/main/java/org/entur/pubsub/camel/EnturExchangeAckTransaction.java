1d82505a156a0ed9795cf82cd80f8876b6c1ee44
vincent <vincent.paturet@entur.org>
2020-05-18 16:15:56 +0200

    Improved error message for missing PubSub ack header

--- camel-entur-google-pubsub/src/main/java/org/entur/pubsub/camel/EnturExchangeAckTransaction.java
+++ camel-entur-google-pubsub/src/main/java/org/entur/pubsub/camel/EnturExchangeAckTransaction.java
@@ -2,6 +2,7 @@ package org.entur.pubsub.camel;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.spi.Synchronization;
+import org.entur.pubsub.base.EnturGooglePubSubException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
@@ -28,12 +29,17 @@ public class EnturExchangeAckTransaction implements Synchronization {
      */
     @Override
     public void onFailure(Exchange exchange) {
-        logger.debug("Acknowledging message after failed processing for Exchang {}", exchange.getExchangeId());
+        logger.debug("Acknowledging message after failed processing for Exchange {}", exchange.getExchangeId());
         getAck(exchange).nack();
     }
 
     private BasicAcknowledgeablePubsubMessage getAck(Exchange exchange) {
-        return ((BasicAcknowledgeablePubsubMessage) exchange.getIn().getHeader(EnturGooglePubSubConstants.ACK_ID));
+        BasicAcknowledgeablePubsubMessage ack = (BasicAcknowledgeablePubsubMessage) exchange.getIn().getHeader(EnturGooglePubSubConstants.ACK_ID);
+        if(ack == null) {
+            String routeId = exchange.getFromRouteId();
+            throw new EnturGooglePubSubException("The exchange does not contain any ack header. Check that the Camel route " + routeId + " does not mistakenly delete it with a call to removeHeaders()");
+        }
+        return ack;
     }
 }
 
