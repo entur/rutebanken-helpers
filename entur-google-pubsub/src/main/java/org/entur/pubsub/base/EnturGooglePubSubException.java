d448b51f6245c99e8f3394a93aed98d283ccb788
vpaturet <46598384+vpaturet@users.noreply.github.com>
2020-05-18 16:30:12 +0200

    Improved error message for missing PubSub ack header (#96)

--- entur-google-pubsub/src/main/java/org/entur/pubsub/base/EnturGooglePubSubException.java
+++ entur-google-pubsub/src/main/java/org/entur/pubsub/base/EnturGooglePubSubException.java
@@ -1,6 +1,11 @@
 package org.entur.pubsub.base;
 
 public class EnturGooglePubSubException extends RuntimeException {
+
+    public EnturGooglePubSubException(String message) {
+        super(message);
+    }
+
     public EnturGooglePubSubException(Exception cause) {
         super(cause);
     }
