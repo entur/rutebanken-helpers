fa5d54bf986c00e6d039d33f023012cd55e8b139
vincent <vincent.paturet@entur.org>
2020-10-21 11:19:12 +0200

    Code cleanup

--- entur-google-pubsub/src/test/java/org/entur/pubsub/base/BasePubSubIntegrationTest.java
+++ entur-google-pubsub/src/test/java/org/entur/pubsub/base/BasePubSubIntegrationTest.java
@@ -1,13 +1,10 @@
 package org.entur.pubsub.base;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApp.class)
 @ActiveProfiles({"google-pubsub-emulator"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
