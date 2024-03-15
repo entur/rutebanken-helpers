4b207ec314762e31a5ce86fe8a40ec4118280b90
Vincent Paturet <vincent.paturet@entur.org>
2024-03-15 14:23:01 +0100

    Add support for OAuth2 multi-issuer resolver

--- oauth2/src/main/java/org/entur/oauth2/AudienceValidator.java
+++ oauth2/src/main/java/org/entur/oauth2/AudienceValidator.java
@@ -5,18 +5,26 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Validate the audience in the JWT token.
  */
 public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
-    private final String audience;
+    private final List<String> audiences;
 
     public AudienceValidator(String audience) {
-        this.audience = audience;
+        this(List.of(audience));
+    }
+
+    public AudienceValidator(List<String> audiences) {
+        Objects.requireNonNull(audiences);
+        this.audiences = audiences;
     }
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
-        if (jwt.getAudience().contains(audience)) {
+        if (audiences.stream().anyMatch(jwt.getAudience()::contains)) {
             return OAuth2TokenValidatorResult.success();
         }
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
