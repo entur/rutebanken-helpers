5508d60ae1df9955fb67f038108bcf20359def69
Vincent Paturet <vincent.paturet@entur.org>
2025-06-26 12:01:37 +0200

    Configure test containers

--- README.md
+++ README.md
@@ -2,30 +2,3 @@
 
 Java code which can be re-used in our different java projects.
 
-
-## Building the libraries
-Some modules depend on [Google PubSub Emulator](https://cloud.google.com/pubsub/docs/emulator) for running unit tests.
-
-[The emulator is not available as a Maven dependency in any public Maven repository](https://stackoverflow.com/questions/38447415/google-pubsub-emulator-maven-repo).
-Options to deal with this dependency include:
-
-1. Adding the emulator to your local Maven proxy under the following Maven GAV:
-```
-<groupId>com.google.cloud</groupId>
-<artifactId>pubsub-emulator</artifactId>
-<version>0.1-SNAPSHOT</version>
-```
-2. Checking in the emulator under source control.
-
-The automatic downloading of the emulator can be deactivated in the Maven build by setting the following property:
-```
-mvn clean install -Dentur.google.pubsub.emulator.download.skip=true
-```
-A Spring property must then be set to point to a custom location for the emulator:
-```
-entur.pubsub.emulator.path=/path/to/emulator/pubsub-emulator-0.1-SNAPSHOT.jar
-```
-3. Disabling altogether unit tests during the build process:
-```
-mvn clean install -DskipTests -Dentur.google.pubsub.emulator.download.skip=true
-```
--- entur-google-pubsub/src/test/java/org/entur/pubsub/base/BasePubSubIntegrationTest.java
+++ entur-google-pubsub/src/test/java/org/entur/pubsub/base/BasePubSubIntegrationTest.java
@@ -1,10 +1,56 @@
 package org.entur.pubsub.base;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PubSubEmulatorContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(classes = TestApp.class)
-@ActiveProfiles({ "google-pubsub-emulator" })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public abstract class BasePubSubIntegrationTest {}
+public abstract class BasePubSubIntegrationTest {
+
+  private static PubSubEmulatorContainer pubsubEmulator;
+
+  @Autowired
+  private EnturGooglePubSubAdmin enturGooglePubSubAdmin;
+
+  @BeforeAll
+  static void init() {
+    pubsubEmulator =
+      new PubSubEmulatorContainer(
+        DockerImageName.parse(
+          "gcr.io/google.com/cloudsdktool/cloud-sdk:emulators"
+        )
+      );
+    pubsubEmulator.start();
+  }
+
+  @AfterAll
+  static void tearDown() {
+    pubsubEmulator.stop();
+  }
+
+  @AfterEach
+  void teardown() {
+    enturGooglePubSubAdmin.deleteAllSubscriptions();
+  }
+
+  @DynamicPropertySource
+  static void emulatorProperties(DynamicPropertyRegistry registry) {
+    registry.add(
+      "spring.cloud.gcp.pubsub.emulator-host",
+      pubsubEmulator::getEmulatorEndpoint
+    );
+    registry.add(
+      "camel.component.google-pubsub.endpoint",
+      pubsubEmulator::getEmulatorEndpoint
+    );
+  }
+}
