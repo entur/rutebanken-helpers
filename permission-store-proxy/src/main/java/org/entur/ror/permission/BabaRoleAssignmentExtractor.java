9b87a94205018407be5cb05a75a1f194e6fce3e1
Vincent Paturet <vincent.paturet@entur.org>
2025-07-04 09:38:53 +0200

    Retrieve internal client permissions from baba

--- permission-store-proxy/src/main/java/org/entur/ror/permission/BabaRoleAssignmentExtractor.java
+++ permission-store-proxy/src/main/java/org/entur/ror/permission/BabaRoleAssignmentExtractor.java
@@ -11,14 +11,10 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
  * Base class for role assignment extractors that retrieve Role assignments from the user repository Baba.
  * Concrete implementations retrieve the role assignments either directly from the database, when running within the
  * Baba service, or through the REST API exposed by Baba.
- * The user is matched by the preferred named claim in the JWT token.
- * In case of a machine-to-machine token, the preferred name is not present in the token and roles are extracted
- * from the Auth0 claim "permissions".
+ * The user is matched by the subject claim in the JWT token.
  */
 public abstract class BabaRoleAssignmentExtractor
   implements RoleAssignmentExtractor {
-
-  private static final String DEFAULT_ADMIN_ORG = "RB";
 
   @Override
   public final List<RoleAssignment> getRoleAssignmentsForUser(
@@ -34,33 +30,10 @@ public abstract class BabaRoleAssignmentExtractor
       jwtAuthenticationToken
     );
 
-    if (authenticatedUser.isInternal()) {
-      return parsePermissionsClaim(authenticatedUser.permissions());
-    }
-
     return userRoleAssignments(authenticatedUser);
   }
 
   protected abstract List<RoleAssignment> userRoleAssignments(
     AuthenticatedUser subject
   );
-
-  /**
-   * Extract RoleAssignments from the permission claim.
-   * Internal tokens (from Entur Internal) contain cross-organization roles under this claim.
-   */
-  private List<RoleAssignment> parsePermissionsClaim(
-    List<String> permissionsClaim
-  ) {
-    return permissionsClaim
-      .stream()
-      .map(role ->
-        RoleAssignment
-          .builder()
-          .withRole(role)
-          .withOrganisation(DEFAULT_ADMIN_ORG)
-          .build()
-      )
-      .toList();
-  }
 }
