48ae186e6196f6c3d74637da3ea1ed35fed39fe7
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-03-27 16:25:07 +0100

    Prettier format

--- slack/src/main/java/org/rutebanken/helper/slack/SlackPostService.java
+++ slack/src/main/java/org/rutebanken/helper/slack/SlackPostService.java
@@ -1,6 +1,5 @@
 package org.rutebanken.helper.slack;
 
-import tools.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -11,52 +10,49 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class SlackPostService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(
-            SlackPostService.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SlackPostService.class);
 
-    private final String slackEndpoint;
-    private final ObjectMapper objectMapper;
+	private final String slackEndpoint;
+	private final ObjectMapper objectMapper;
 
-    @Autowired
-    public SlackPostService(
-            @Value("${helper.slack.endpoint}") String slackEndpoint) {
-        this.slackEndpoint = slackEndpoint;
-        objectMapper = new ObjectMapper();
-    }
+	@Autowired
+	public SlackPostService(@Value("${helper.slack.endpoint}") String slackEndpoint) {
+		this.slackEndpoint = slackEndpoint;
+		objectMapper = new ObjectMapper();
+	}
 
-    public boolean publish(String messageText) {
-        SlackPayload payload = new SlackPayload(messageText);
-        return publish(payload);
-    }
+	public boolean publish(String messageText) {
+		SlackPayload payload = new SlackPayload(messageText);
+		return publish(payload);
+	}
 
-    public boolean publish(SlackPayload payload) {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            String json = objectMapper.writeValueAsString(payload);
-            LOGGER.debug("Will try to send the following to slack: {}", json);
-            HttpPost httpPost = new HttpPost(slackEndpoint);
-            httpPost.setEntity(new StringEntity(json));
-            httpPost.addHeader("Content-Type", "application/json");
-            httpclient.execute(
-                    httpPost,
-                    response -> {
-                        if (LOGGER.isInfoEnabled()) {
-                            LOGGER.info(
-                                    "Entity as response from hubot: {}",
-                                    EntityUtils.toString(response.getEntity()));
-                        }
-                        return null;
-                    });
-            return true;
-        } catch (Exception e) {
-            LOGGER.error("Could not publish message", e);
-            return false;
-        }
-    }
+	public boolean publish(SlackPayload payload) {
+		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			String json = objectMapper.writeValueAsString(payload);
+			LOGGER.debug("Will try to send the following to slack: {}", json);
+			HttpPost httpPost = new HttpPost(slackEndpoint);
+			httpPost.setEntity(new StringEntity(json));
+			httpPost.addHeader("Content-Type", "application/json");
+			httpclient.execute(
+				httpPost,
+				response -> {
+					if (LOGGER.isInfoEnabled()) {
+						LOGGER.info("Entity as response from hubot: {}", EntityUtils.toString(response.getEntity()));
+					}
+					return null;
+				}
+			);
+			return true;
+		} catch (Exception e) {
+			LOGGER.error("Could not publish message", e);
+			return false;
+		}
+	}
 
-    public record SlackPayload(String text) {
-    }
+	public record SlackPayload(String text) {}
 }
